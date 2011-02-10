2c434e990a4e0e97f862b9cbc09937a2280bc030
jsomers <jsomers@gmail.com>
2011-02-09 20:17:36 -0500

    Initial Rails 3 layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizdash::Application.config.secret_token = '3e5aab1674ec19d1689818371349569c1dc6393dc512a51e444031758ea85e89fd30ee34c4791f388518543c33d0a1c2e4dffd1dcae03e15238df989c5292876'
